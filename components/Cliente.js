38d7bd7e37193c13cbd360b9420eae2430f0e986
Miguel Maidana <maidana.miguell@gmail.com>
2020-06-15 14:07:10 -0300

    Componenete Editar Cliente. Routing para ir EditarCliente. Creo el formulario. Query para obtener el cliente segun el id

--- components/Cliente.js
+++ components/Cliente.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Swal from "sweetalert2"
 import {gql,useMutation} from "@apollo/client"
+import Router from "next/router"
 
 const ELIMINAR_CLIENTE = gql`
     mutation eliminarCliente($id:ID!){
@@ -84,6 +85,13 @@ const Cliente = ({cliente}) => {
           })
         
     }
+
+    const editarCliente =(id)=>{
+        Router.push({
+            pathname : "/editarcliente/[id]",
+            query : {id}
+        })
+    }
     return (  
         <tr key={cliente.id}>
               <td className="border px-4 py-2">{cliente.nombre} {cliente.apellido}</td>
@@ -97,6 +105,16 @@ const Cliente = ({cliente}) => {
                   >
                       Eliminar
                       <svg fill="none" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" viewBox="0 0 24 24" stroke="currentColor" className="w-4 h-4 ml-2"><path d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg>
+                  </button>
+              </td>
+              <td className="border px-4 py-2">
+                  <button
+                    type="button"
+                    className="flex justify-center items-center bg-green-600 py-2 px-4 w-full text-white rounded text-xs uppercase font-bold"
+                    onClick={()=>editarCliente(cliente.id)}
+                  >
+                      Editar
+                      <svg fill="none" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" viewBox="0 0 24 24" stroke="currentColor" className="w-4 h-4 ml-2"><path d="M11 5H6a2 2 0 00-2 2v11a2 2 0 002 2h11a2 2 0 002-2v-5m-1.414-9.414a2 2 0 112.828 2.828L11.828 15H9v-2.828l8.586-8.586z"></path></svg>
                   </button>
               </td>
               
---
+++ pages/editarcliente/[pid].js
@@ -0,0 +1,159 @@
+import React from 'react';
+import {useRouter} from "next/router"
+import Layout from "../../components/Layout"
+import {gql,useQuery} from "@apollo/client"
+
+const OBTENER_CLIENTE = gql`
+query obternerCliente($id:ID!){
+    obternerCliente(id:$id){
+        nombre
+        apellido
+        empresa
+        email
+        telefono
+    }
+}
+`
+
+const EditarCliente = () => {
+
+    
+
+    //obtener id Actual
+    const router = useRouter()
+    const {query:{id}} = router
+    console.log(id)
+
+    //Query para consultar a BD
+
+    const {data,loading,error} = useQuery(OBTENER_CLIENTE,{
+        variables:{
+            id
+        }
+    })
+
+    console.log(data)
+    console.log(loading)
+    console.log(error)
+
+    return (
+        <Layout>
+         <h1  className="text-2xl text-gray-800 font-light">Editar Cliente</h1>
+         <div className="flex justify-center mt-5">
+                        <div className="w-full max-w-lg">
+                            <form 
+                                className="bg-white shadow-md px-8 pt-6 pb-8 mb-4"
+                                //onSubmit={formik.handleSubmit}
+                            >
+                                <div className="mb-4">
+                                    <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="nombre">
+                                        Nombre
+                                    </label>
+                                    <input 
+                                    className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                                    id="nombre"
+                                    type="text"
+                                    placeholder="Nombre Cliente"
+                                    // value={formik.values.nombre}
+                                    // onChange={formik.handleChange}
+                                    // onBlur={formik.handleBlur}
+                                    />
+                                </div>
+                                    {/* {formik.touched.nombre && formik.errors.nombre ?(
+                                        <div className="my-2 bg-red-200 border-l-4 border-red-500 text-red-700 p-4">
+                                        <p className="font-bold">Error</p>
+                                        <p>{formik.errors.nombre}</p>
+                                        </div>
+                                    ):null} */}
+                                <div className="mb-4">
+                                    <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="apellido">
+                                        Apellido
+                                    </label>
+                                    <input 
+                                    className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                                    id="apellido"
+                                    type="text"
+                                    placeholder="Apellido Cliente"
+                                    // value={formik.values.apellido}
+                                    // onChange={formik.handleChange}
+                                    // onBlur={formik.handleBlur}
+                                    />
+                                </div>
+                                    {/* {formik.touched.apellido && formik.errors.apellido ?(
+                                        <div className="my-2 bg-red-200 border-l-4 border-red-500 text-red-700 p-4">
+                                        <p className="font-bold">Error</p>
+                                        <p>{formik.errors.apellido}</p>
+                                        </div>
+                                    ):null} */}
+                                <div className="mb-4">
+                                    <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="empresa">
+                                        Empresa
+                                    </label>
+                                    <input 
+                                    className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                                    id="empresa"
+                                    type="text"
+                                    placeholder="Empresa Cliente"
+                                    // value={formik.values.empresa}
+                                    // onChange={formik.handleChange}
+                                    // onBlur={formik.handleBlur}
+                                    />
+                                </div>
+                                    {/* {formik.touched.empresa && formik.errors.empresa ?(
+                                        <div className="my-2 bg-red-200 border-l-4 border-red-500 text-red-700 p-4">
+                                        <p className="font-bold">Error</p>
+                                        <p>{formik.errors.empresa}</p>
+                                        </div>
+                                    ):null} */}
+                                <div className="mb-4">
+                                    <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="email">
+                                        Email
+                                    </label>
+                                    <input 
+                                    className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                                    id="email"
+                                    type="email"
+                                    placeholder="Email Cliente"
+                                    // value={formik.values.email}
+                                    // onChange={formik.handleChange}
+                                    // onBlur={formik.handleBlur}
+                                    />
+                                </div>
+                                      {/* {formik.touched.email && formik.errors.email ?(
+                                        <div className="my-2 bg-red-200 border-l-4 border-red-500 text-red-700 p-4">
+                                        <p className="font-bold">Error</p>
+                                        <p>{formik.errors.email}</p>
+                                        </div>
+                                    ):null} */}
+                                <div className="mb-4">
+                                    <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="telefono">
+                                        Telefono
+                                    </label>
+                                    <input 
+                                    className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                                    id="telefono"
+                                    type="tel"
+                                    placeholder="Telefono Cliente"
+                                    // value={formik.values.telefono}
+                                    // onChange={formik.handleChange}
+
+
+                                   
+                                    />
+                                </div>
+                                <input 
+                                    type="submit"
+                                    className="bg-gray-800 w-full mt-5 p-2 text-white uppercase hover:bg-gray-700"
+                                    value="Editar Cliente"
+                                 />
+                            </form>
+
+                        </div>
+                    </div>
+
+
+        </Layout>
+      );
+}
+ 
+export default EditarCliente;
