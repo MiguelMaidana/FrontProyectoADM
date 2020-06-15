63fb1b5ce8230a0729f15cb8ebb3ab99c8716e08
Miguel Maidana <maidana.miguell@gmail.com>
2020-06-15 15:55:12 -0300

    Componente Productos. Listo los componentes con el Query. Doy formato al formulario. Funcionalidad completa del boton eliminar

--- pages/productos.js
+++ pages/productos.js
@@ -1,11 +1,63 @@
 import React from 'react';
 import Layout from '../components/Layout';
+import Producto from "../components/Producto"
+import {gql, useQuery} from "@apollo/client"
+import Link from "next/router"
+
+
+const OBTENER_PRODUCTOS= gql`
+  query obtenerProductos{
+    obtenerProductos{
+      id
+      nombre
+      existencia
+      precio
+    }
+  }
+`
 
 const Productos = () => {
+
+  //Query para Obtener Productos
+
+  const{data,loading,error} = useQuery(OBTENER_PRODUCTOS)
+
+  if(loading) return "Cargando..."
     return ( 
-        <Layout>
-      <h1 className="text-2xl text-gray-800 font-light">Productos</h1>
+
+      <div>
+          <Layout>
+            <h1 className="text-2xl text-gray-800 font-light">Productos</h1>
+            {/* <Link href="/nuevoproducto">
+              <a className="bg-blue-800 py-2 px-5 mt-5 inline-block text-white rounded text-sm hover:bg-gray-800 mb-3 uppercase font-bold">Nuevo Cliente</a>
+            </Link> */}
+
+            <table className="table-auto shadow-md mt-10 w-full w-lg">
+              <thead className="bg-gray-800">
+                <tr className="text-white">
+                  <th className="w-1/5 py-2">Nombre</th>
+                  <th className="w-1/5 py-2">Existencia</th>
+                  <th className="w-1/5 py-2">Precio</th>
+                  <th className="w-1/5 py-2">Elminar</th>
+                  <th className="w-1/5 py-2">Editar</th>
+
+                </tr>
+
+              </thead>
+          <tbody className="bg-white">
+          {data.obtenerProductos.map(producto=>(
+            <Producto
+              key={producto.id}
+              producto={producto}
+            >
+
+            </Producto>
+          ))}
+          </tbody>
+
+        </table>
         </Layout>
+      </div>
      );
 }
  
